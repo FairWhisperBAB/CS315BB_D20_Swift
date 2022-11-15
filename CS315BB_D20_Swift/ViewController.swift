ebea32c0d501dd5c238698a12a33a0189b48770f
Benjamin Briggs <91103093+FairWhisperBAB@users.noreply.github.com>
2022-11-15 08:19:29 -0600

    Initial Commit

---
+++ CS315BB_D20_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS315BB_D20_Swift
+//
+//  Created by Benjamin Briggs on 11/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
