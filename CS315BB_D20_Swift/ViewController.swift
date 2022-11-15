483ac735ab3a45c2c0395891aa6684e716adce44
Benjamin Briggs <91103093+FairWhisperBAB@users.noreply.github.com>
2022-11-15 09:19:48 -0600

    Basic dice button working

--- CS315BB_D20_Swift/ViewController.swift
+++ CS315BB_D20_Swift/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var diceImageView: UIImageView!
+    @IBOutlet var criticalLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func buttonGotPressed()
+    {
+        rollDice()
+    }
+    
+    func rollDice()
+    {
+        print("THEY SEE YOU ROLLIN, THEY HATIN!!")
+        
+        let rollNumber = Int.random(in: 1...20)
+        
+        let imageName = "d\(rollNumber)"
+        
+        diceImageView.image = UIImage(named: imageName)
+    }
 
 }
 
